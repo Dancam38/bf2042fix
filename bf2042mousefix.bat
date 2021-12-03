58a7fce53df1fe77c30293e57ca222ceb5626c4c
Dancam38 <64447060+Dancam38@users.noreply.github.com>
2021-12-02 17:53:15 -0800

    Add files via upload

---
+++ bf2042mousefix.bat
@@ -0,0 +1,6 @@
+set message = this program will delete PROFSAVE and PROFSAVE_profile from the bf2042 directory in documents to fix the ingame mouse yaw issue.
+echo %message%
+pause
+del "C:\Users\%USERNAME%\Documents\Battlefield 2042\settings\PROFSAVE_profile"
+del "C:\Users\%USERNAME%\Documents\Battlefield 2042\settings\PROFSAVE"
+
